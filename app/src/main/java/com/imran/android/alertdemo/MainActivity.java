350132eb6cccb4060218cf4f95b955461d52efd1
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-13 01:59:52 +0600

    dialog button

--- app/src/main/java/com/imran/android/alertdemo/MainActivity.java
+++ app/src/main/java/com/imran/android/alertdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.imran.android.alertdemo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are you Sure!?")
+                .setMessage("Do you definitely want this?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 }
